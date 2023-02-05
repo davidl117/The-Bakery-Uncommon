c621a9f8424347bc8d9247bc9c3ef90edf9c4df8
David Lasky <lasky96@gmail.com>
2023-02-05 16:37:02 -0500

    added animation to burger bar

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -4,18 +4,28 @@ import { Link } from "react-router-dom";
 
 export default function Navigation() {
 
+    const [burger_bar, setBurgerClass] = useState("burger-bar unclicked");
     const [menuOpen, setMenuOpen] = useState(false);
-    
+    const [menu_class, setMenuClass] = useState("menu-hidden");
+
     function toggleMenu() {
+        if (!menuOpen) {
+            setBurgerClass("burger-bar clicked")
+            setMenuClass("menu-open")
+        }else {
+            setBurgerClass("burger-bar unclicked")
+            setMenuClass("menu-hidden")
+        }
         setMenuOpen(!menuOpen)
     }
+    console.log(menuOpen);
 
     return (
         <div>
             <div className="ham-bars" onClick={toggleMenu}>
-                <div className="burger-bar"></div>
-                <div className="burger-bar"></div>
-                <div className="burger-bar"></div>
+                <div className={burger_bar}></div>
+                <div className={burger_bar}></div>
+                <div className={burger_bar}></div>
             </div>            
             <h1 className="title">The Bakery Uncommon</h1>
             <p className="shopping-cart">Cart</p>
